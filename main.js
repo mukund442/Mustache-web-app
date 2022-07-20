fd10c0a970116d7f08acb381592516094a33735e
mukund442 <92722963+mukund442@users.noreply.github.com>
2022-07-20 16:54:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function preload(){
+    mustache=loadImage('https://i.postimg.cc/brrbZCsD/mustache.png')
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(mustache, under_nose_x, under_nose_y, )
 }
 
 function setup(){
@@ -27,7 +29,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x=" +results[0].pose.nose.x);
-        console.log("nose y=" +results[0].pose.nose.y);
+        under_nose_x=results[0].pose.nose.x-40;
+        under_nose_y=results[0].pose.nose.y-40;
+        console.log("nose x=" +under_nose_x);
+        console.log("nose y=" +under_nose_y);
     }
 }
+
+under_nose_x=0;
+under_nose_y=0;
